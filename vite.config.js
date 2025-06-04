503674e273ff4d1404667c2f881a4b9bac79f94b
antonio iadicicco <justtoni2@gmail.com>
2025-06-03 21:26:20 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/antoniox200.github.io/',
+  base: '/antoniox200.github.io/',
   build: {
     rollupOptions: {
       output: {
